7c80469c36973729883501a2ede1fb4fe087916b
anjukacado1 <anjukacado1@gmail.com>
2024-02-19 17:47:02 +0530

    files added

---
+++ practice-01/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Demo{
+    public void printmsg(){
+        Console.WriteLine("hello");
+    }
+       public void printmsg22(){
+        Console.WriteLine("hello22");
+    }
+}
+
+
+class Practice
+{
+    static void Main(string [] args)
+    {
+        Console.WriteLine("Welcome to Great karikalan magic show.");
+        Console.WriteLine("Hello, World!");
+        Demo d=new Demo();
+        d.printmsg();
+        d.printmsg22();
+        printmsg1();
+         printmsg12();
+        
+
+    }
+
+     public static void printmsg1(){
+        Console.WriteLine("hello1");
+    }
+    public static void printmsg12(){
+        Console.WriteLine("hello12");
+    }
+}
+
+// class Demopractice{
+//     public void printmsg3(string[] args){
+//         string name="Akil";
+//         Console.WriteLine(name);
+//     }
+// }
+
+
+// static string returnMsgMethod(){
+//     string name="cadbatch01R";
+//     return name;
+// //    Console.WriteLine(name);
+// }
+
+
+
+static void(string[] args){
+
+string name="akil";
+Console.WriteLine(name);
+
+}
