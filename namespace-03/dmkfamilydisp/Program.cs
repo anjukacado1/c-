ee46fe4cf39ab369e5f2b11123e7cf9183e194cf
anjukacado1 <anjukacado1@gmail.com>
2024-02-27 16:00:02 +0530

    namespae created.

---
+++ namespace-03/dmkfamilydisp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace dmkfamilydisp{
+
+    class Program{
+        static void Main(string [] args){
+            Console.WriteLine("Welcome to dmk family");
+            Kalaingar kr=new Kalaingar();
+
+        }
+
+    }
+
+}
---
+++ namespace-03/dmkfamilydisp/kalaingar.cs
@@ -0,0 +1,9 @@
+namespace dmkfamilydisp{
+ class Kalaingar
+ {
+        public Kalaingar(){
+            Console.WriteLine("I am kalaingar.");
+        }
+}
+
+}
