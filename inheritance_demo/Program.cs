3b661b17ffcb3da43b267c1d727dc7a87c2104ae
anjukacado1 <anjukacado1@gmail.com>
2024-02-27 16:02:50 +0530

    inheritance file created.

---
+++ inheritance_demo/Program.cs
@@ -0,0 +1,24 @@
+﻿class drivedclass3{
+    public int derimet3(int a){
+        return a*10;
+    }
+}
+class derivedclass2:drivedclass3{
+    public int derimet2(int a){
+        return derimet3(a+10);
+    }
+}
+class drivedclass:drivedclass3{
+    public int derimet(int a){
+        return derimet3(a+15);
+    }
+}
+class  Program{
+    static void Main(String[] args){
+        drivedclass obj = new drivedclass();        
+        derivedclass2 obj1 = new derivedclass2();        
+
+        Console.WriteLine(obj1.derimet2(10));
+
+    }
+}
